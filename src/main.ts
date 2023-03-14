6a9c25a6ccd31e7ce6025ab2aa790b5206e9c6b9
Keagon98 <keaggzb20v@gmail.com>
2023-03-14 21:22:35 +0200

    Setup some of the project file structure and added more dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css';
+
+createApp(App).mount('#app');
+
+import 'bootstrap/dist/js/bootstrap.js';
